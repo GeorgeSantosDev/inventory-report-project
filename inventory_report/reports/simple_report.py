876fe946d5ccbec22a15793c9d72510b5459d88b
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-04 18:22:41 -0300

    feat: Create a generate class method

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,42 @@
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, report_list: list[dict]) -> str:
+        today_date = datetime.today().date()
+        manufacturing_dates = []
+        expiration_dates = []
+        producers = dict()
+
+        for product in report_list:
+            manufacturing_date = datetime.strptime(
+                product["data_de_fabricacao"], "%Y-%m-%d"
+            ).date()
+            expiration_date = datetime.strptime(
+                product["data_de_validade"], "%Y-%m-%d"
+            ).date()
+
+            manufacturing_dates.append(manufacturing_date)
+
+            if expiration_date > today_date:
+                expiration_dates.append(expiration_date)
+
+            if product["nome_da_empresa"] in producers:
+                producers[product["nome_da_empresa"]] += 1
+            else:
+                producers[product["nome_da_empresa"]] = 1
+
+        if len(expiration_dates) > 0:
+            closest_date = min(expiration_dates)
+        else:
+            closest_date = ''
+
+        earliest_date = min(manufacturing_dates)
+        company_with_more_products = max(producers, key=producers.get)
+
+        return (
+            f'Data de fabricação mais antiga: {earliest_date}\n'
+            f'Data de validade mais próxima: {closest_date}\n'
+            f'Empresa com mais produtos: {company_with_more_products}'
+        )
