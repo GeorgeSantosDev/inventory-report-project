985b0e4b3a889eca11f77fbfd92c9d283fb82344
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-04 19:03:39 -0300

    feat: Create generate method for CompleteReport class

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, report_list: list[dict]) -> str:
+        response = super().generate(report_list)
+        producers = dict()
+
+        for product in report_list:
+            if product["nome_da_empresa"] in producers:
+                producers[product["nome_da_empresa"]] += 1
+            else:
+                producers[product["nome_da_empresa"]] = 1
+
+        response += "\nProdutos estocados por empresa:\n"
+
+        for key, value in producers.items():
+            response += f"- {key}: {value}\n"
+        print(response)
+        return response
