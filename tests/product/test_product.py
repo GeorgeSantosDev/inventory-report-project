2d59b3fe0cfff9433b87e76b5f1f6bdd87901b15
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-05 11:07:37 -0300

    test: Refactor test to pass in trybe evaluator

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,8 +1,17 @@
-from tests.factories.product_factory import ProductFactory
+from inventory_report.inventory.product import Product
+
+
+expected = (
+    'O produto D '
+    'fabricado em 2002-05-12 '
+    'por AZ com validade '
+    'até 2019-05-12 '
+    'precisa ser armazenado air.'
+)
 
 
 def test_cria_produto():
-    product = ProductFactory()
+    product = Product(1, "D", "AZ", "2002-05-12", "2019-05-12", 122, "air")
     assert isinstance(product.id, int)
     assert isinstance(product.nome_do_produto, str)
     assert isinstance(product.nome_da_empresa, str)
@@ -10,3 +19,4 @@ def test_cria_produto():
     assert isinstance(product.data_de_validade, str)
     assert isinstance(product.numero_de_serie, int)
     assert isinstance(product.instrucoes_de_armazenamento, str)
+    assert repr(product) == expected
