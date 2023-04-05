90bdc43d7eb722e454c65fbe8ab3581169932ffd
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-05 19:46:33 -0300

    feat: Apply Iterator Design Pattern in Inventory class

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,15 @@
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+from collections.abc import Iterable
+
+
+class InventoryRefactor(Iterable):
+    def __init__(self, importer) -> None:
+        self.importer = importer
+        self.data = []
+
+    def import_data(self, path: str, report_type) -> None:
+        new_data = self.importer.import_data(path)
+        self.data = [*self.data, *new_data]
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
