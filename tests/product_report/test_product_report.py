460e1fa837dc1ca254c24d6f4ff136f05e6165b7
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-05 11:34:04 -0300

    test: Create test por repr method of Product class

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+expected = (
+    "O produto farinha "
+    "fabricado em 2021-05-01 "
+    "por Farinini com validade "
+    "até 2023-06-02 "
+    "precisa ser armazenado ao abrigo de luz."
+)
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "farinha",
+        "Farinini",
+        "2021-05-01",
+        "2023-06-02",
+        122,
+        "ao abrigo de luz"
+    )
+    assert repr(product) == expected
